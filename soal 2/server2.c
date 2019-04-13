c2f308132a699932c43e17898409c27b87214888
Claus Peter <34019306+clausaxon@users.noreply.github.com>
2019-04-13 21:30:35 +0700

    Revisi
    revisi bagian penambahan thread dan timer

--- soal 2/server2.c
+++ soal 2/server2.c
@@ -1,24 +1,35 @@
+
 #include <stdio.h>
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 #include <unistd.h>
-#define PORT 8080
+#include <pthread.h>
+#define PORT 9090
+
+void* print(void* arg){
+    while(1){
+        int num = *((int *) arg);
+        printf("Stok : %d\n", num);
+        sleep(5);
+    }    
+}
 
 int main(int argc, char const *argv[]) {
     int server_fd, new_socket, valread;
     struct sockaddr_in address;
     int opt = 1;
+    char *hello = "Stok Bertambah";
     int addrlen = sizeof(address);
-    char buffer[1024] = {0};
-    char *hello;
-      
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
-      
+
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
         perror("setsockopt");
         exit(EXIT_FAILURE);
@@ -27,7 +38,7 @@ int main(int argc, char const *argv[]) {
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons( PORT );
-      
+
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
@@ -42,28 +53,30 @@ int main(int argc, char const *argv[]) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    
-    //shared memory
+
     key_t key = 1234;
-        int *value;
+    int *value;
 
-        int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
-        value = shmat(shmid, NULL, 0);
+    int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+    value = shmat(shmid, NULL, 0);
 
+    int *p=value;
+    pthread_t tid1;
+    pthread_create(&tid1, NULL, print, (void*) p);
+
+    while(1){
+        char buffer[1024] = {0};
         valread = read( new_socket , buffer, 1024);
-        if(strcmp(buffer,"beli")){
-            *value-=1;
+        printf("Penjual %s Stok\n",buffer );
+        if(strcmp(buffer, "tambah")==0 || strcmp(buffer, "Tambah")==0){
+            *value += 1;
+            send(new_socket , hello , strlen(hello) , 0 );
+            printf("Stok Bertambah\n");
         }
-        
-        shmdt(value);
-        shmctl(shmid, IPC_RMID, NULL);shared memory
 
+    }
 
-    //end shared memory    
-
-    
-    printf("%s\n",buffer );
-    send(new_socket , hello , strlen(hello) , 0 );
-    printf("Hello message sent\n");
+    shmdt(value);
+    shmctl(shmid, IPC_RMID, NULL);
     return 0;
 }
